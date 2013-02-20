714fa0cbdad7daf65b28e67fc05e6febe6e325e0
Dale Campbell <oshuma@gmail.com>
2013-02-20 04:27:01 -0500

    List and subscriber specs

--- spec/interspire/api_spec.rb
+++ spec/interspire/api_spec.rb
@@ -6,27 +6,54 @@ describe Interspire::API do
     @api_url = 'http://example.com/xml.php'
     @user = 'luser'
     @token = 'some_totally_valid_token'
+
+    @api = Interspire::API.new(@api_url, @user, @token)
   end
 
   context 'authentication' do
     it 'should be authenticated' do
       stub_request(:post, @api_url).to_return(:body => fixture("authenticated.xml"))
 
-      @api = Interspire::API.new(@api_url, @user, @token)
       @api.should be_authenticated  # @api.authenticated? should return true
     end
 
     it 'should not be authenticated' do
       stub_request(:post, @api_url).to_return(:body => fixture("not_authenticated.xml"))
 
-      @api = Interspire::API.new(@api_url, @user, @token)
       @api.should_not be_authenticated  # @api.authenticated? should return false
     end
   end
 
   context 'lists' do
-    it 'should get all lists'
-    it 'should get all subscribers on a list'
+    it 'should get all lists' do
+      stub_request(:post, @api_url).to_return(:body => fixture("lists.xml"))
+
+      lists = @api.get_lists
+
+      # Make sure each list has expected values:
+      lists.each do |list|
+        list[:id].should_not be_nil
+        list[:name].should_not be_nil
+        list[:subscribe_count].should_not be_nil
+        list[:unsubscribe_count].should_not be_nil
+        list[:auto_responder_count].should_not be_nil
+      end
+    end
+
+    it 'should get all subscribers on a list' do
+      stub_request(:post, @api_url).to_return(:body => fixture("subscribers.xml"))
+
+      subscribers = @api.get_subscribers(1)  # Contact list ID is arbitrary for this test.
+
+      # Make sure there's a count.
+      subscribers[:count].should_not be_nil
+
+      # Make sure the required values are there:
+      subscribers[:subscribers].each do |subscriber|
+        subscriber[:subscriber_id].should_not be_nil
+        subscriber[:email].should_not be_nil
+      end
+    end
   end
 
   context 'subscribers' do
